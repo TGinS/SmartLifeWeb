9414b16571865453e77be78608ba0fe53d2aeecf
TG <tg@a-no-iMac.local>
2015-10-30 03:09:00 +0900

    optimize navbar logined

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
-  devise_for :users
   root 'welcome#index'
+
+  devise_for :users do
+    get '/help',    :on => :collection
+  end
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
