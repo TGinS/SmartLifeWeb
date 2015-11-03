0002eb4662275a20b5edd31cb368ede7f3447895
TG <tg@a-no-iMac.local>
2015-11-03 17:05:44 +0900

    add vote count in show page

--- app/controllers/invitation_controller.rb
+++ app/controllers/invitation_controller.rb
@@ -2,9 +2,12 @@ class InvitationController < ApplicationController
   def show
     id = params[:id]
     @invitation = Invitation.joins(:user).select("invitations.*, users.email AS user_email").find(id)
-    @invitation_candidates = InvitationCandidate.find_by_sql(['SELECT provisions.id AS provision_id, provisions.name
-                                                            FROM invitation_candidates, provisions
+    @invitation_candidates = InvitationCandidate.find_by_sql(['SELECT provisions.id AS provision_id, provisions.name, COUNT(*) AS vote
+                                                            FROM invitation_candidates, provisions, invitation_candidate_votes
                                                             WHERE invitation_candidates.provision_id = provisions.id
-                                                            AND invitation_candidates.invitation_id = ?',id])
+                                                            AND invitation_candidates.id = invitation_candidate_votes.invitation_candidate_id
+                                                            AND invitation_candidates.invitation_id = ?
+                                                            GROUP BY provision_id, provisions.name',id])
+
   end
 end
