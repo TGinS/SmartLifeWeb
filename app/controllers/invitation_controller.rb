31452d4bc47874b80589c44ed3bbf3eb819cd71e
TG <tg@a-no-iMac.local>
2015-11-03 15:08:35 +0900

    edit show page

--- app/controllers/invitation_controller.rb
+++ app/controllers/invitation_controller.rb
@@ -1,4 +1,10 @@
 class InvitationController < ApplicationController
   def show
+    id = params[:id]
+    @invitation = Invitation.joins(:user).select("invitations.*, users.email AS user_email").find(id)
+    @invitation_candidates = InvitationCandidate.find_by_sql(['SELECT provisions.id AS provision_id, provisions.name
+                                                            FROM invitation_candidates, provisions
+                                                            WHERE invitation_candidates.provision_id = provisions.id
+                                                            AND invitation_candidates.invitation_id = ?',id])
   end
 end
--- app/controllers/provision_controller.rb
+++ app/controllers/provision_controller.rb
@@ -1,4 +1,10 @@
 class ProvisionController < ApplicationController
   def show
+    id = params[:id]
+    @provision = Provision.joins(:user).select("provisions.*, users.email AS user_email").find(id)
+    @provision_candidates = ProvisionCandidate.find_by_sql(['SELECT invitations.id AS invitation_id, invitations.name
+                                                            FROM provision_candidates, invitations
+                                                            WHERE provision_candidates.invitation_id = invitations.id
+                                                            AND provision_candidates.provision_id = ?',id])
   end
 end
