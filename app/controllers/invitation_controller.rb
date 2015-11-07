d1f7b0f25d1f5974cc864f4b8924de431896a548
TG <tg@a-no-iMac.local>
2015-11-07 18:04:09 +0900

    changed invitation params

--- app/controllers/invitation_controller.rb
+++ app/controllers/invitation_controller.rb
@@ -7,7 +7,7 @@ class InvitationController < ApplicationController
   end
 
   def create
-    invitation = params.require(:candidate).permit(:name,:invitation_type_id,:user_id,:latitude,:longitude,:note,:end_date)
+    invitation = params.require(:invitation).permit(:name,:invitation_type_id,:user_id,:latitude,:longitude,:note,:end_date)
     invitation = Invitation.new(invitation)
     if invitation.save
       redirect_to :back, :flash=>{:result=>"追加しました"}
